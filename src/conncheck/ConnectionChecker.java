90eb4eda0840457d774a51772c4c52682dd7a50e
torresmateo <torresmateo@localhost>
2012-08-05 15:32:06 -0400

    Lista la clase que verifica las conecciones

--- src/conncheck/ConnectionChecker.java
+++ src/conncheck/ConnectionChecker.java
@@ -1,7 +1,11 @@
 package conncheck;
 
+import java.net.InetSocketAddress;
 import java.net.Socket;
-
+import java.net.SocketTimeoutException;
+/**
+ * Clase que hace el checkeo de la conecccion
+ */
 public class ConnectionChecker {
 	private Socket socket;
 	private int port;
@@ -9,14 +13,40 @@ public class ConnectionChecker {
 	ConnectionChecker(String host, int port){
 		this.host = host;
 		this.port = port;
-		try{
-			socket = new Socket(this.host, this.port);
-		}catch(Exception e){
+	}
+	//verifica la coneccion
+	public boolean Check(){
+		boolean returnValue = true;
+		try{//tratamos de conectarnos
+			socket = new Socket();
+			socket.connect(new InetSocketAddress(this.host, this.port),1000);
+		}catch(SocketTimeoutException e){//handleamos el fallo en que no se pudo conectar por timeout
+			System.out.println("La coneccion no se pudo establecer, se llega al timeout");
 			e.printStackTrace();
+			returnValue = false;
+		}catch(Exception e){//otro tipo de errores
+			e.printStackTrace();
+			returnValue = false;
+		}finally{//cerramos el soket
+			if (socket != null){
+				try{
+					socket.close();
+				}catch(Exception e){//en caso en que el cierre de socket falle
+					e.printStackTrace();
+					returnValue = false;
+				}
+			}
 		}
+		return returnValue;
 	}
 	
-	public boolean Check(){
-		return true;
+	//ejemplo del uso de la clase
+	public static void main(String args[]){
+		ConnectionChecker connCheck = new ConnectionChecker("www.google.com",80);
+		if(connCheck.Check()){
+			System.out.println("hay coneccion");
+		}else{
+			System.out.println("NO hay coneccion");
+		}
 	}
 }
