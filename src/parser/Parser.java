c1682169b79cbfbc78a41bc77b7652ade30f2081
samucg89 <samucg89@localhost>
2012-08-06 20:06:09 -0400

    Modificaciones para optimizacion de FileManager y Parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -3,6 +3,8 @@ package parser;
 import java.io.*;
 import java.util.*;
 
+import utils.ServerProperties;
+
 public class Parser{
 	
 	public Properties prop;
@@ -29,24 +31,24 @@ public class Parser{
 		return prop;
 	}
 	
-	public void writeProperties(HashMap<utils.ServerProperties, String> data){
+	public void writeProperties(HashMap<ServerProperties, String> data){
 		
 		try {
 			OutputStream output = new FileOutputStream(this.file);
 
-			prop.put("address", data.get(utils.ServerProperties.ADDRESS));
-			prop.put("alias", data.get(utils.ServerProperties.ALIAS));
-			prop.put("check_interval", data.get(utils.ServerProperties.CHECK_INTERVAL));
-			prop.put("current_state", data.get(utils.ServerProperties.CURRENT_STATE));
-			prop.put("email_notification", data.get(utils.ServerProperties.EMAIL_NOTIF));
-			prop.put("host_name", data.get(utils.ServerProperties.HOSTNAME));
-			prop.put("last_check", data.get(utils.ServerProperties.LAST_CHECK));
-			prop.put("last_notificacion", data.get(utils.ServerProperties.LAST_NOTIF));
-			prop.put("max_check_attempts", data.get(utils.ServerProperties.MAX_CHECK_ATTEMPTS));
-			prop.put("notification_interval", data.get(utils.ServerProperties.NOTIF_INTERVAL));
-			prop.put("ports_list", data.get(utils.ServerProperties.PORTS_LIST));
-			prop.put("retry_interval", data.get(utils.ServerProperties.RETRY_INTERVAL));
-			prop.put("tolerance_attempts", data.get(utils.ServerProperties.TOLERANCE_ATTEMPTS));
+			prop.put("address", data.get(ServerProperties.ADDRESS));
+			prop.put("alias", data.get(ServerProperties.ALIAS));
+			prop.put("check_interval", data.get(ServerProperties.CHECK_INTERVAL));
+			prop.put("current_state", data.get(ServerProperties.CURRENT_STATE));
+			prop.put("email_notification", data.get(ServerProperties.EMAIL_NOTIF));
+			prop.put("host_name", data.get(ServerProperties.HOSTNAME));
+			prop.put("last_check", data.get(ServerProperties.LAST_CHECK));
+			prop.put("last_notification", data.get(ServerProperties.LAST_NOTIF));
+			prop.put("max_check_attempts", data.get(ServerProperties.MAX_CHECK_ATTEMPTS));
+			prop.put("notification_interval", data.get(ServerProperties.NOTIF_INTERVAL));
+			prop.put("ports_list", data.get(ServerProperties.PORTS_LIST));
+			prop.put("retry_interval", data.get(ServerProperties.RETRY_INTERVAL));
+			prop.put("tolerance_attempts", data.get(ServerProperties.TOLERANCE_ATTEMPTS));
 			
 			prop.store(output, "server properties");
 			
