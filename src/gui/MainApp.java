928a0112c4e49e06ceba59f24cd351d0e4079b1a
samucg89 <samucg89@localhost>
2012-08-06 20:05:27 -0400

    GUI con add, edit, delete de Propiedades del servidor

--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -20,13 +20,18 @@ import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 import conncheck.ServerMonitor;
 
 
 import parser.Directory;
+import parser.FileManager;
 import parser.Parser;
 
 /*
@@ -35,12 +40,9 @@ import parser.Parser;
 
 public class MainApp extends JFrame {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -408641277064642917L;
 
-	public static final String DIR_PATH="/Users/torresmateo/Documents/workspace/tp-java-2012/servers";
+	public static final String DIR_PATH="/home/samu/java_workspace/tp-java-2012/servers/";
 	
 	JPanel basic;         //panel principal de la App
 	JTabbedPane tabpanel; //panel de tabs de la App
@@ -50,13 +52,21 @@ public class MainApp extends JFrame {
 	JTree tree;           //tree de los servers
 	DefaultMutableTreeNode root; //nodo raiz tree
 	DefaultTreeModel model; //modelo de datos del tree
+	Hashtable<String, Object> htTree; //Hashtable para el Tree de los servers
+	Hashtable<String, Properties> serverData; //
+	
+	String lastSelectedServerName; 
+	DefaultMutableTreeNode lastSelectedNode;
 	
 	JToolBar toolbar;     //toolbar de la App
-	AddServerDialog dialog; //Ventana de dialogo 
-	Hashtable<String, Object> htTree; //Hashtable para el Tree de los servers
+	AddServerDialog AddDialog; //Ventana de dialogo de Add
+	EditServerDialog EditDialog; //Ventana de dialogo de edit
+	
 
     public MainApp() {
-    	dialog = new AddServerDialog(this); //dialogo modal
+    	AddDialog = new AddServerDialog(this); //dialogo modal
+    	EditDialog = new EditServerDialog(this); //dialogo modal
+    	serverData = new Hashtable<String, Properties>();
         initUI();
     }
 
@@ -100,19 +110,109 @@ public class MainApp extends JFrame {
         toolbar.add(configButton);
         toolbar.add(exitButton);
         toolbar.setFloatable(false);
+        
+        
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
+			@Override
+			public void valueChanged(TreeSelectionEvent e) {
+				 lastSelectedNode = (DefaultMutableTreeNode)
+                         tree.getLastSelectedPathComponent();
+				 
+				 if (lastSelectedNode == null || lastSelectedNode.isRoot()){
+					 lastSelectedServerName = null;
+					 return;
+				 }
+				 
+				 if(lastSelectedNode.isLeaf()){
+					 lastSelectedServerName = lastSelectedNode.getParent().toString();
+					// System.out.println(node.getParent().toString());
+				 }
+				 else{
+					 lastSelectedServerName = lastSelectedNode.toString();
+					// System.out.println(node.toString()); 
+				 }	 
+			}
+		});
 
         //Anonymous inner class
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
             	 SwingUtilities.invokeLater(new Runnable() {
                      public void run() {
-                         dialog.setVisible(true);
+                    	 AddDialog.setVisible(true);
                          //como es modal, al llegar aquí es porque se ha cerrado la
                          //ventana
-                         if(dialog.newServerProp()!=null){         
+                         if(AddDialog.newServerProp()!=null){         
                         	 //cargar nuevo server file properties
-                        	 addNewServerFileToTree();
+                        	 addNewServerFileToTree(AddDialog);
                          }
+                     }
+                 });
+            }
+        });
+        
+        editButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+            	 SwingUtilities.invokeLater(new Runnable() {
+                     public void run() {
+                    	 if(lastSelectedNode==null){
+                    		 JOptionPane.showMessageDialog(basic, "You must " +
+                    		 		"select a server first!",
+                                     "Error", JOptionPane.ERROR_MESSAGE);
+                    		 return;
+                    	 }
+                    	 if(lastSelectedNode.isRoot()){
+                    		 JOptionPane.showMessageDialog(basic, "Can't " +
+                    		 		"edit root",
+                                     "Error", JOptionPane.ERROR_MESSAGE);
+                    		 return;
+                    	 }
+                    	 EditDialog.loadFields();
+                         EditDialog.setVisible(true);
+                         //como es modal, al llegar aquí es porque se ha cerrado la
+                         //ventana
+                         if(EditDialog.newServerProp()!=null){         
+                        	 //cargar nuevo server file properties
+                        	 if(lastSelectedNode.isLeaf()){
+	                    		 ((DefaultTreeModel) tree.getModel()).
+	                        	 removeNodeFromParent((MutableTreeNode) lastSelectedNode.getParent()); 	
+                    		 }
+                    		 else{
+                    			 ((DefaultTreeModel) tree.getModel()).
+	                        	 removeNodeFromParent(lastSelectedNode);
+                    		 }
+                        	 addNewServerFileToTree(EditDialog);	
+                         }
+                         
+                     }
+                 });
+            }
+        });
+        
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+            	 SwingUtilities.invokeLater(new Runnable() {
+                     public void run() {
+                    	 if(lastSelectedNode.isRoot()){
+                    		 JOptionPane.showMessageDialog(basic, "Can't delete root!",
+                                     "Error", JOptionPane.ERROR_MESSAGE);
+                    		 return;
+                    	 }                  	 
+                    	 int opt = JOptionPane.showConfirmDialog(basic,"Are you " +
+                    	 		"sure you want to delete \""+getLastSelectedServerName()+"\"?",
+                        		 "Exit",JOptionPane.YES_NO_OPTION);
+                    	 if(opt==0){
+                    		 serverData.remove(getLastSelectedServerName());
+                    		 FileManager.remove(getLastSelectedServerName());
+                    		 if(lastSelectedNode.isLeaf()){
+	                    		 ((DefaultTreeModel) tree.getModel()).
+	                        	 removeNodeFromParent((MutableTreeNode) lastSelectedNode.getParent()); 	
+                    		 }
+                    		 else{
+                    			 ((DefaultTreeModel) tree.getModel()).
+	                        	 removeNodeFromParent(lastSelectedNode);
+                    		 }
+                    	 }
                      }
                  });
             }
@@ -134,7 +234,7 @@ public class MainApp extends JFrame {
         basic.add(tabpanel,BorderLayout.CENTER);
 
         setTitle("Service monitoring");
-        setSize(800, 600);
+        setSize(900, 700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -157,22 +257,41 @@ public class MainApp extends JFrame {
 		// Leer los datos de cada archivo de configuración
 		for (int i = 0; i < servers_file.length; i++) {
 			Parser p = new Parser(servers_file[i].getAbsolutePath());
+			serverData.put(servers_file[i].getName(),p.readProperties());
 			htTree.put(servers_file[i].getName(),propertiesToStringArray(p.readProperties()));
 			//TODO iniciar el hilo de monitor de este server
 			ServerMonitor server = new ServerMonitor(p.readProperties());
 			server.start();
-			Properties prop = p.readProperties();
-			prop.get("address");
 		} 
 		
 		JTree.DynamicUtilTreeNode.createChildren(root, htTree);
 		tree = new JTree(model);
+		tree.getSelectionModel().setSelectionMode
+          (TreeSelectionModel.SINGLE_TREE_SELECTION);
 
 	    JScrollPane scrollPane = new JScrollPane(tree);
 	    serverPanel.add(scrollPane, BorderLayout.CENTER);
 	 
 		return serverPanel;
 	}
+    
+    /*
+     * Creación del panel de la Tabla de email
+     */
+    
+    public JPanel createEmailPanel(String text) {
+		JPanel emailPanel = new JPanel();
+		JLabel jlbDisplay = new JLabel(text);
+		jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
+		emailPanel.setLayout(new GridLayout(1, 1));
+		emailPanel.add(jlbDisplay);
+		return emailPanel;
+	}
+    
+    /*
+     * Others
+     */
+
     
     public String[] propertiesToStringArray(Properties prop){
     	String[] StringProp = new String[13];
@@ -185,10 +304,11 @@ public class MainApp extends JFrame {
 		return StringProp;	
     }
     
-    public void addNewServerFileToTree(){
+    public void addNewServerFileToTree(ServerDialog sdialog){
     	 Hashtable<String, Object> ht = new Hashtable<String, Object>();
-    	 ht.put(dialog.newServerName(),
-    	    propertiesToStringArray(dialog.newServerProp()));
+    	 ht.put(sdialog.newServerName(),
+    	    propertiesToStringArray(sdialog.newServerProp()));
+    	 serverData.put(sdialog.newServerName(), sdialog.newServerProp());
     	 JTree.DynamicUtilTreeNode.createChildren(root,ht);
     	 ((DefaultTreeModel) tree.getModel()).reload();
     	 
@@ -197,17 +317,20 @@ public class MainApp extends JFrame {
     }
     
     /*
-     * Creacion del panel de la Tabla de email
+     * Funciones para comunicarse con JDialog
      */
     
-    public JPanel createEmailPanel(String text) {
-		JPanel emailPanel = new JPanel();
-		JLabel jlbDisplay = new JLabel(text);
-		jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
-		emailPanel.setLayout(new GridLayout(1, 1));
-		emailPanel.add(jlbDisplay);
-		return emailPanel;
-	}
+    public String getLastSelectedServerName(){
+    	return lastSelectedServerName;
+    }
+    
+    public Hashtable<String, Properties> getServerData(){
+    	return serverData;
+    }
+    
+    /*
+     * Java application
+     */
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
---
+++ src/gui/ServerDialog.java
@@ -0,0 +1,183 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.util.HashMap;
+import java.util.Properties;
+
+import javax.swing.Box;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import parser.Parser;
+
+import utils.ServerProperties;
+
+
+public class ServerDialog extends JDialog{
+	
+	 JPanel basic;  //panel principal de la App
+	 
+	 JLabel hintTitle; //hint title
+	 //Campos del formulario
+     JTextField tfaddress;
+     JTextField tfalias;
+     JTextField tfcheckint;
+     JTextField tfcurrentstate;
+     JTextField tfemailnotif;
+     JTextField tfhostname;
+     JTextField tflastcheck;
+     JTextField tflastnotif;
+     JTextField tfmaxcheckattempts;
+     JTextField tfnotifinterval;
+     JTextField tfportslist;
+     JTextField tfretryint;
+     JTextField tftolerance;
+     
+     //panel de botones
+     JPanel bottom;
+     
+     //Hashmap para almacenar la información del formulario
+     HashMap<ServerProperties, String> data;
+     
+     boolean new_save;
+
+    public ServerDialog(JFrame father) {
+    	//ventana modal
+    	super(father,true);
+        initUI();
+        
+    }
+
+    public void initUI() {
+
+        basic = new JPanel();
+        basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+        add(basic);
+
+        JPanel topPanel = new JPanel(new BorderLayout(0, 0));
+        topPanel.setMaximumSize(new Dimension(450, 0));
+        hintTitle = new JLabel("");
+        hintTitle.setBorder(BorderFactory.createEmptyBorder(10, 15, 0, 0));
+        topPanel.add(hintTitle);
+
+        ImageIcon icon = new ImageIcon(getClass().getResource("server.png"));
+        JLabel label = new JLabel(icon);
+        label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        topPanel.add(label, BorderLayout.WEST);
+
+        JSeparator separator = new JSeparator();
+        separator.setForeground(Color.DARK_GRAY);
+
+        topPanel.add(separator, BorderLayout.SOUTH);
+
+        basic.add(topPanel);
+
+        //Formulario
+        JPanel PropPanel = new JPanel(new GridLayout(13, 2, 1, 5));
+        PropPanel.setBorder(BorderFactory.createEmptyBorder(15, 25, 15, 25));
+        
+        JLabel laddress = new JLabel("Address:");
+        tfaddress = new JTextField("",15);
+        JLabel lalias = new JLabel("Alias:");
+        tfalias = new JTextField("",15);
+        JLabel lcheckint = new JLabel("Check interval:");
+        tfcheckint = new JTextField("",15);
+        JLabel lcurrentstate = new JLabel("Current state:");
+        tfcurrentstate = new JTextField("",15);
+        JLabel lemailnotif = new JLabel("Email notification:");
+        tfemailnotif = new JTextField("",15);
+        JLabel lhostname = new JLabel("Host name:");
+        tfhostname = new JTextField("",15);
+        JLabel llastcheck = new JLabel("Last check:");
+        tflastcheck = new JTextField("",15);
+        JLabel llastnotif = new JLabel("Last notification:");
+        tflastnotif = new JTextField("",15);
+        JLabel lmaxcheckattempts = new JLabel("Max check attempts:");
+        tfmaxcheckattempts = new JTextField("",15);
+        JLabel lnotifinterval = new JLabel("Notification interval:");
+        tfnotifinterval = new JTextField("",15);
+        JLabel lportslist = new JLabel("Ports list:");
+        tfportslist = new JTextField("",15);
+        JLabel lretryint = new JLabel("Retry interval:");
+        tfretryint = new JTextField("",15);
+        JLabel ltolerance = new JLabel("Tolerance attempts	:");
+        tftolerance = new JTextField("",15);
+        
+        PropPanel.add(laddress); PropPanel.add(tfaddress);
+        PropPanel.add(lalias);   PropPanel.add(tfalias);
+        PropPanel.add(lcheckint);PropPanel.add(tfcheckint);
+        PropPanel.add(lcurrentstate);PropPanel.add(tfcurrentstate);
+        PropPanel.add(lemailnotif);PropPanel.add(tfemailnotif);
+        PropPanel.add(lhostname);PropPanel.add(tfhostname);
+        PropPanel.add(llastcheck);PropPanel.add(tflastcheck);
+        PropPanel.add(llastnotif);PropPanel.add(tflastnotif);
+        PropPanel.add(lmaxcheckattempts);PropPanel.add(tfmaxcheckattempts);
+        PropPanel.add(lnotifinterval);PropPanel.add(tfnotifinterval);
+        PropPanel.add(lportslist);PropPanel.add(tfportslist);
+        PropPanel.add(lretryint);PropPanel.add(tfretryint);
+        PropPanel.add(ltolerance);PropPanel.add(tftolerance);
+
+        basic.add(PropPanel);
+
+        //Botones
+        bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        basic.add(bottom);
+
+        setTitle("");
+        setSize(new Dimension(450, 600));
+        setResizable(false);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    
+    public void cleanFields(){
+          tfaddress.setText("");
+          tfalias.setText("");
+          tfcheckint.setText("");
+          tfcurrentstate.setText("");
+          tfemailnotif.setText("");
+          tfhostname.setText("");
+          tflastcheck.setText("");
+          tflastnotif.setText("");
+          tfmaxcheckattempts.setText("");
+          tfnotifinterval.setText("");
+          tfportslist.setText("");
+          tfretryint.setText("");
+          tftolerance.setText("");
+    }
+    
+    /*
+     * Funciones para comunicación con ventana padre
+     */
+    
+    public Properties newServerProp(){
+    	if(new_save){
+    		String server_file = MainApp.DIR_PATH+data.get(ServerProperties.ALIAS)
+    				+".properties";	
+    		Parser p = new Parser(server_file);
+			return p.readProperties();
+    	}
+    	return null;
+    }
+    
+    public String newServerName(){
+    	if(new_save){
+    		String server_file = data.get(ServerProperties.ALIAS)+".properties";	
+    		return server_file;
+    	}
+    	return null;
+    }
+    
+}
